ce743bc07c4374d8edfc28a23fdcd393918ea734
angelocat <catalaniangelo@gmail.com>
2021-08-28 19:07:14 +0200

    tests: ssh connections against a containerized ssh server

---
+++ tests/ssh_client.rs
@@ -0,0 +1,43 @@
+use std::net::TcpStream;
+
+use ssh2::Session;
+use std::io::Read;
+
+#[test]
+fn connect_to_ssh_server_with_username_password() {
+    let tcp = TcpStream::connect("0.0.0.0:2222").unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_password("test_user", "test_password")
+        .unwrap();
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("<{}>", s);
+    channel.wait_close().ok();
+    println!("<{}>", channel.exit_status().unwrap());
+}
+
+#[test]
+fn connect_to_ssh_server_with_private_key() {
+    let tcp = TcpStream::connect("0.0.0.0:2222").unwrap();
+    let mut sess = Session::new().unwrap();
+    sess.set_tcp_stream(tcp);
+    sess.handshake().unwrap();
+    sess.userauth_pubkey_file(
+        "test_user",
+        None,
+        "tests/config/private_key/id_rsa".as_ref(),
+        None,
+    )
+    .unwrap();
+    let mut channel = sess.channel_session().unwrap();
+    channel.exec("ls").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    println!("<{}>", s);
+    channel.wait_close().ok();
+    println!("<{}>", channel.exit_status().unwrap());
+}
