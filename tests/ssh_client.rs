c3e69c6061c848a83fbb45388312531256022889
angelocat <catalaniangelo@gmail.com>
2021-08-29 12:31:19 +0200

    feat(ssh_client): working skeleton with tests

--- tests/ssh_client.rs
+++ tests/ssh_client.rs
@@ -3,41 +3,24 @@ use std::net::TcpStream;
 
 use ssh2::Session;
 
+use ssh_client::{AuthMethod, SSHClient};
+
 #[test]
-fn connect_to_ssh_server_with_username_password() {
-    let tcp = TcpStream::connect("0.0.0.0:2222").unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-    sess.userauth_password("test_user", "test_password")
-        .unwrap();
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("ls").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("<{}>", s);
-    channel.wait_close().ok();
-    println!("<{}>", channel.exit_status().unwrap());
+fn ssh_client_connects_to_ssh_server_with_username_password() {
+    let c = SSHClient::new(
+        "0.0.0.0:2222",
+        "test_user",
+        AuthMethod::from_password("test_password"),
+    );
+    c.run_command("ls");
 }
 
 #[test]
-fn connect_to_ssh_server_with_private_key() {
-    let tcp = TcpStream::connect("0.0.0.0:2222").unwrap();
-    let mut sess = Session::new().unwrap();
-    sess.set_tcp_stream(tcp);
-    sess.handshake().unwrap();
-    sess.userauth_pubkey_file(
+fn ssh_client_connects_to_ssh_server_with_private_key() {
+    let c = SSHClient::new(
+        "0.0.0.0:2222",
         "test_user",
-        None,
-        "configuration/private_key/id_rsa".as_ref(),
-        None,
-    )
-    .unwrap();
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("ls").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    println!("<{}>", s);
-    channel.wait_close().ok();
-    println!("<{}>", channel.exit_status().unwrap());
+        AuthMethod::from_private_key("configuration/private_key/id_rsa"),
+    );
+    c.run_command("ls");
 }
